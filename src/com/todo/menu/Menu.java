5b20368a4b3a14698efbb4091bfe0a2ebe141dcc
Chodabeen <22000711@handong.edu>
2021-09-26 19:27:52 +0900

    update displaymenu

--- src/com/todo/menu/Menu.java
+++ src/com/todo/menu/Menu.java
@@ -5,14 +5,15 @@ public class Menu {
     {
         System.out.println();
         System.out.println("<ToDoList 앱 명령어 사용법>");
-        System.out.println("1. add - 항목 추가");
-        System.out.println("2. del - 항목 삭제");
-        System.out.println("3. edit - 항목 수정");
-        System.out.println("4. ls - 전체 목록");
-        System.out.println("5. ls_name_asc - 제목순 정렬");
-        System.out.println("6. ls_name_desc - 제목역순 정렬");
-        System.out.println("7. ls_date - 날짜순 정렬");
-        System.out.println("8. exit - 종료");
+        System.out.println("add - 항목 추가");
+        System.out.println("del - 항목 삭제");
+        System.out.println("edit - 항목 수정");
+        System.out.println("ls - 전체 목록");
+        System.out.println("ls_name_asc - 제목순 정렬");
+        System.out.println("ls_name_desc - 제목역순 정렬");
+        System.out.println("ls_date - 날짜순 정렬");
+        System.out.println("find <검색어> - 항목 검색");
+        System.out.println("exit - 종료");
     }
     
     public static void prompt()
